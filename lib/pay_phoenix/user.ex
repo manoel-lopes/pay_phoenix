8957c71d493d30b9ebb783506107b6f13ea76ffb
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-17 17:46:44 -0300

    refactoring the regex for email format validation

--- lib/pay_phoenix/user.ex
+++ lib/pay_phoenix/user.ex
@@ -33,7 +33,7 @@ defmodule PayPhoenix.User do
     |> validate_required(@required_params)
     |> validate_length(:password, min: 8)
     |> validate_number(:age, greater_than_or_equal_to: 18)
-    |> validate_format(:email, ~r/@/)
+    |> validate_format(:email, ~r/[a-z0-9]+@[a-z0-9]+\.com/)
     |> unique_constraint([:email])
     |> unique_constraint([:nickname])
     |> put_password_hash()
