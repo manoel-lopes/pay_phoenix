960f3c079b337ae386635e9a78d959231ac8a4be
manoel-lopes <manoafonso.lopes@hotmail.com>
2021-04-17 16:58:40 -0300

    refactoring call function of PayPhoenix.Users.Store module to create a user with a account

--- lib/pay_phoenix/users/store.ex
+++ lib/pay_phoenix/users/store.ex
@@ -1,12 +1,12 @@
 defmodule PayPhoenix.Users.Store do
   alias Ecto.Multi
-  alias PayPhoenix.{ Account, Repo, User}
+  alias PayPhoenix.{Account, Repo, User}
 
   def call(params) do
     Multi.new()
     |> Multi.insert(:create_user, User.changeset(params))
     |> Multi.run(:create_account, fn repo, %{create_user: user} ->
-      insert_account(repo, user)
+      inser_account(repo, user)
     end)
     |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
       preload_data(repo, user)
@@ -14,15 +14,14 @@ defmodule PayPhoenix.Users.Store do
     |> run_transaction()
   end
 
-  defp insert_account(repo, user) do
+  defp inser_account(repo, user) do
     user.id
-    |> account_changeset()
+    |> build_account_chageset()
     |> repo.insert()
   end
 
-  defp account_changeset(user_id) do
+  defp build_account_chageset(user_id) do
     params = %{user_id: user_id, balance: "0.00"}
-
     Account.changeset(params)
   end
 
@@ -32,7 +31,9 @@ defmodule PayPhoenix.Users.Store do
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
-      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:error, _operation, reason, _changes} ->
+        {:error, reason}
+
       {:ok, %{preload_data: user}} -> {:ok, user}
     end
   end
